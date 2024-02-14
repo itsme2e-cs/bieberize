31ca0e100eb6d7a91bcb4ff0c3462e339a393442
itsme2e-cs <160049494+itsme2e-cs@users.noreply.github.com>
2024-02-14 11:21:43 -0600

    Update bieberize.ps1

--- bieberize.ps1
+++ bieberize.ps1
@@ -1,9 +1,87 @@
+ <# 
+    Powershell Script to bring the user back to the early 2000s with some classic Bieber. 
+    Uses Jose Espita's Set-Wallpaper Function (https://www.joseespitia.com/2017/09/15/set-wallpaper-powershell-function/)
+#>
+
+Function Set-WallPaper {
+ 
+<#
+ 
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
+    
+    .PARAMETER Image
+    Provide the exact path to the image
+ 
+    .PARAMETER Style
+    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+  
+    .EXAMPLE
+    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+  
+#>
+ 
+param (
+    [parameter(Mandatory=$True)]
+    # Provide path to image
+    [string]$Image,
+    # Provide wallpaper style that you would like applied
+    [parameter(Mandatory=$False)]
+    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+    [string]$Style
+)
+ 
+$WallpaperStyle = Switch ($Style) {
+  
+    "Fill" {"10"}
+    "Fit" {"6"}
+    "Stretch" {"2"}
+    "Tile" {"0"}
+    "Center" {"0"}
+    "Span" {"22"}
+  
+}
+ 
+If($Style -eq "Tile") {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+ 
+}
+Else {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+ 
+}
+ 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
 
 # Download CLASSIC Bieber
 invoke-webrequest "https://cdn.britannica.com/70/145070-050-B810D3E1/Justin-Bieber.jpg" -OutFile "$HOME\Downloads\bieber.jpg"
 
-# set Reg Key for current user desktop wallpaper
-set-itemproperty -path "HKCU:Control Panel\Desktop" -name Wallpaper -value "$HOME\Downloads\bieber.jpg"
+# Invoke Set-Wallpaper Function
+Set-WallPaper -Image "$HOME\Downloads\bieber.jpg" -Style Fit 
 
-# Update user system parameters to avoid having to reboot to get new wallapper
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
